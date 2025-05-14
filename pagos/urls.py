7fa24ba4e15ae4c440624b3dd091142e6dc9ee34
EdgarISC2021 <isc_ebarajas2021@accitesz.com>
2025-05-14 09:33:01 -0600

    Delete pagos directory

--- pagos/urls.py
+++
@@ -1,9 +0,0 @@
-# urls.py
-from rest_framework import routers
-from rest_framework.routers import DefaultRouter
-from .api import PagoViewSet
-
-router = routers.DefaultRouter()
-router.register(r'api/pagos', PagoViewSet, basename='pagos')
-
-urlpatterns = router.urls
